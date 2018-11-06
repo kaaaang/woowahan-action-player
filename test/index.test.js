21c15f9b1c5d070982a9f886272e8d20e1a5a971
river-stone-jin <sukjinjjang@woowahan.com>
2018-11-06 13:56:19 +0900

    테스트 서술에 대한 피드백 반영

--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,7 @@ import ViewConstructor from './view';
 import { hasAllRequiredOptionAction } from './mock-data/input.action';
 
 describe('initialize action player', () => {
-    test('view has actions object', () => {
-        const view = ViewConstructor(hasAllRequiredOptionAction);
-
-        expect(typeof view.actions).toBe('object');
-    });
-
-    test('woowahan-action-player make action dispatcher function', () => {
+    test('woowahan-action-player should make action dispatcher function from actions object', () => {
         const view = ViewConstructor(hasAllRequiredOptionAction);
 
         const actions = Object.keys(view.actions);
@@ -18,7 +12,7 @@ describe('initialize action player', () => {
         });
     });
 
-    test('action apply multiple tasks', () => {
+    test('should dispatch multiple action when tasks propety type is array', () => {
         const TASK_1 = 'task 1';
         const TASK_2 = 'task 2';
         const TASK_3 = 'task 3';
@@ -55,26 +49,7 @@ describe('initialize action player', () => {
         view.multipleTasksAction();
     });
 
-    test('has immediate option', () => {
-        const finish = jest.fn();
-        const actions = {
-            immediateAction: {
-                immediate: true,
-                tasks: {
-                    name: 'immediate action',
-                    params: {
-
-                    }
-                },
-                finish
-            }
-        }
-        const view = ViewConstructor(actions);
-
-        expect(view.actions.immediateAction).toHaveProperty('immediate');
-    });
-
-    test('immediate excute action player when immediate option is true', () => {
+    test('should immediate dispatch action when immediate option is true', () => {
         const finish = jest.fn();
         const actions = {
             immediateAction: {
@@ -98,51 +73,7 @@ describe('initialize action player', () => {
         expect(view.actions.immediateAction.immediate).toBeTruthy();
     });
 
-    test('action has sequence option', () => {
-        const ACTION_1 = 'sequence action 1';
-        const ACTION_2 = 'sequence action 2';
-        const ACTION_3 = 'sequence action 3';
-
-        const actions = {
-            sequenceActions: {
-                sequence: true,
-                tasks: [{
-                    name: ACTION_1,
-                    params(data = {}) {
-                        const newData = {};
-                        newData.no = !!data.no ? data.no + 1 : 1;
-
-                        return newData;
-                    }
-                }, {
-                    name: ACTION_2,
-                    params(data = {}) {
-                        const newData = {};
-                        newData.no = !!data.no ? data.no + 1 : 1;
-
-                        return newData;
-                    }
-                }, {
-                    name: ACTION_3,
-                    params(data = {}) {
-                        const newData = {};
-                        newData.no = !!data.no ? data.no + 1 : 1;
-
-                        return newData;
-                    }
-                }],
-                finish(results) {
-                    expect(results[ACTION_1].resp.no).toBe(1);
-                    expect(results[ACTION_2].resp.no).toBe(2);
-                    expect(results[ACTION_3].resp.no).toBe(3);
-                }
-            }
-        };
-
-        expect(actions.sequenceActions).toHaveProperty('sequence');
-    });
-
-    test('tasks success sequence when sequence option true', () => {
+    test('should tasks dispatch sequence when sequence option true', () => {
         const ACTION_1 = 'sequence action 1';
         const ACTION_2 = 'sequence action 2';
         const ACTION_3 = 'sequence action 3';
@@ -190,27 +121,7 @@ describe('initialize action player', () => {
         view.sequenceActions();
     });
 
-    test('has preventDuplicateCall option', () => {
-        const finish = jest.fn();
-        const actions = {
-            preventDupliacteCallAction: {
-                preventDuplicateCall: true,
-                tasks: {
-                    name: 'immediate action',
-                    params() {
-                        finish();
-
-                        expect(finish).toHaveBeenCalledTimes(1);
-                    }
-                },
-                finish() {}
-            }
-        }
-
-        expect(actions.preventDupliacteCallAction).toHaveProperty('preventDuplicateCall');
-    });
-
-    test('prevent dupliacate action call when preventDupliacteCall option is true', () => {
+    test('should prevent dupliacate dispatch same action when preventDupliacteCall option is true', () => {
         const finish = jest.fn();
         const actions = {
             preventDupliacteCallAction: {
